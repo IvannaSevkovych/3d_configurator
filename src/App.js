b04944405f1f3fdc6f60cb7d0c63d4eb72f9b07c
Ivanna Sevkovych <is@wacg.de>
2021-05-06 12:17:36 +0200

    Add custom model and change materials

--- src/App.js
+++ src/App.js
@@ -1,38 +1,41 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState, Suspense } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 
-function Box(props) {
-  // This reference will give us direct access to the mesh
-  const mesh = useRef()
-  // Set up state for the hovered and active state
-  const [hovered, setHover] = useState(false)
-  const [active, setActive] = useState(false)
-  // Rotate mesh every frame, this is outside of React without overhead
-  useFrame(() => {
-    mesh.current.rotation.x = mesh.current.rotation.y += 0.01
-  })
-  return (
-    <mesh
-      {...props}
-      ref={mesh}
-      scale={active ? 1.5 : 1}
-      onClick={(e) => setActive(!active)}
-      onPointerOver={(e) => setHover(true)}
-      onPointerOut={(e) => setHover(false)}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-    </mesh>
-  )
+import { useGLTF } from '@react-three/drei'
+
+function Arch(props) {
+    const group = useRef()
+    const { nodes, materials } = useGLTF('/arch.glb')
+    return (
+        <group ref={group} {...props} dispose={null}>
+            <group position={[0, -0.04, 0]}>
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.arch_1.geometry}
+                >
+                    <meshStandardMaterial color='orange' />
+                </mesh>
+
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.arch_2.geometry}
+                >
+                    <meshStandardMaterial color='green' />
+                </mesh>
+            </group>
+        </group>
+    )
 }
 
 export default function App() {
-  return (
-    <Canvas>
-      <ambientLight intensity={0.5} />
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-      <pointLight position={[-10, -10, -10]} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
-    </Canvas>
-  )
+    return (
+        <Canvas>
+            <ambientLight intensity={0.5} />
+            <Suspense fallback={null}>
+                <Arch />
+            </Suspense>
+        </Canvas>
+    )
 }
