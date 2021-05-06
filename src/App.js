d6c15058a0f65a5a02791a9dd9a5815775b78eee
Ivanna Sevkovych <is@wacg.de>
2021-05-06 18:04:43 +0200

    Add colour picker

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { useRef, useState, Suspense } from 'react'
+import React, { useRef, useState, Suspense, useEffect } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 
 import { useGLTF, OrbitControls } from '@react-three/drei'
+import { HexColorPicker } from "react-colorful"
 import { proxy, useSnapshot } from 'valtio'
 
 
@@ -18,15 +19,22 @@ function Arch(props) {
     const snap = useSnapshot(state)
     const { nodes, materials } = useGLTF('/arch.glb')
     const [hovered, set] = useState(null)
+
+    useEffect(() => {
+        // document.body.style.cursor = hovered ? "pointer" : "auto"
+        const cursor = `<svg width="64" height="64" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0)"><path fill="rgba(255, 255, 255, 0.5)" d="M29.5 54C43.031 54 54 43.031 54 29.5S43.031 5 29.5 5 5 15.969 5 29.5 15.969 54 29.5 54z" stroke="#000"/><g filter="url(#filter0_d)"><path d="M29.5 47C39.165 47 47 39.165 47 29.5S39.165 12 29.5 12 12 19.835 12 29.5 19.835 47 29.5 47z" fill="${snap.items[hovered]}"/></g><path d="M2 2l11 2.947L4.947 13 2 2z" fill="#000"/><text fill="#000" style="white-space:pre" font-family="Inter var, sans-serif" font-size="10" letter-spacing="-.01em"><tspan x="35" y="63">${hovered}</tspan></text></g><defs><clipPath id="clip0"><path fill="#fff" d="M0 0h64v64H0z"/></clipPath><filter id="filter0_d" x="6" y="8" width="47" height="47" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="BackgroundImageFix"/><feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/><feOffset dy="2"/><feGaussianBlur stdDeviation="3"/><feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.15 0"/><feBlend in2="BackgroundImageFix" result="effect1_dropShadow"/><feBlend in="SourceGraphic" in2="effect1_dropShadow" result="shape"/></filter></defs></svg>`
+        const auto = `<svg width="64" height="64" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill="rgba(255, 255, 255, 0.5)" d="M29.5 54C43.031 54 54 43.031 54 29.5S43.031 5 29.5 5 5 15.969 5 29.5 15.969 54 29.5 54z" stroke="#000"/><path d="M2 2l11 2.947L4.947 13 2 2z" fill="#000"/></svg>`
+        document.body.style.cursor = `url('data:image/svg+xml;base64,${btoa(hovered ? cursor : auto)}'), auto`
+    })
+
     return (
         <group ref={group} {...props} dispose={null}>
             <group
                 position={[0, -0.04, 0]}
                 onPointerOver={(e) => { e.stopPropagation(), set(e.object.material.name) }}
                 onPointerOut={(e) => { e.intersections.length === 0 && set(null) }}
-                onPointerDown={(e) => { e.stopPropagation; state.current = e.object.material.name }}
+                onPointerDown={(e) => { e.stopPropagation, state.current = e.object.material.name }}
                 onPointerMissed={(e) => { state.current = null }}
-                onPointerOver={(e) => { e.stopPropagation(), console.log(e.object.material.name) }}
             >
                 <mesh
                     castShadow
@@ -52,14 +60,21 @@ function Arch(props) {
 function Picker() {
     const snap = useSnapshot(state)
     return (
-        <div className="picker">{snap.current}</div>
+        <div style={{ display: snap.current ? "block" : "none" }}>
+            <HexColorPicker
+                className="picker"
+                color={snap.items[snap.current]}
+                onChange={(color) => (state.items[snap.current] = color)}
+            />
+            <h1>{snap.current}</h1>
+        </div>
     )
 }
 
 export default function App() {
     return (
         <>
-            <Picker/>
+            <Picker />
             <Canvas>
                 <ambientLight intensity={0.5} />
                 <Suspense fallback={null}>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
 import './styles.css'
+import "react-colorful/dist/index.css"
 import App from './App'
 
 ReactDOM.render(<App />, document.getElementById('root'))
