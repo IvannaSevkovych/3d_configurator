e4bfb71afd2147372f1fa3fe0ae8c617e7d07b49
Ivanna Sevkovych <is@wacg.de>
2021-05-06 16:33:47 +0200

    Update valtio, make material name show up on click

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { useRef, useState, Suspense } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 
 import { useGLTF, OrbitControls } from '@react-three/drei'
-import { proxy, useProxy } from 'valtio'
+import { proxy, useSnapshot } from 'valtio'
+
 
 const state = proxy({
     current: null,
@@ -14,39 +15,58 @@ const state = proxy({
 
 function Arch(props) {
     const group = useRef()
-    const snap = useProxy(state)
+    const snap = useSnapshot(state)
     const { nodes, materials } = useGLTF('/arch.glb')
+    const [hovered, set] = useState(null)
     return (
         <group ref={group} {...props} dispose={null}>
-            <group position={[0, -0.04, 0]}>
+            <group
+                position={[0, -0.04, 0]}
+                onPointerOver={(e) => { e.stopPropagation(), set(e.object.material.name) }}
+                onPointerOut={(e) => { e.intersections.length === 0 && set(null) }}
+                onPointerDown={(e) => { e.stopPropagation; state.current = e.object.material.name }}
+                onPointerMissed={(e) => { state.current = null }}
+                onPointerOver={(e) => { e.stopPropagation(), console.log(e.object.material.name) }}
+            >
                 <mesh
                     castShadow
                     receiveShadow
                     geometry={nodes.arch_1.geometry}
                 >
-                    <meshStandardMaterial color={snap.items.house} />
+                    <meshStandardMaterial name="house" color={snap.items.house} />
                 </mesh>
+
 
                 <mesh
                     castShadow
                     receiveShadow
                     geometry={nodes.arch_2.geometry}
                 >
-                    <meshStandardMaterial color={snap.items.heart} />
+                    <meshStandardMaterial name="heart" color={snap.items.heart} />
                 </mesh>
             </group>
         </group>
     )
 }
 
+function Picker() {
+    const snap = useSnapshot(state)
+    return (
+        <div className="picker">{snap.current}</div>
+    )
+}
+
 export default function App() {
     return (
-        <Canvas>
-            <ambientLight intensity={0.5} />
-            <Suspense fallback={null}>
-                <Arch />
-            </Suspense>
-            <OrbitControls enablePan={true} enableZoom={true} enableRotate={true} />
-        </Canvas>
+        <>
+            <Picker/>
+            <Canvas>
+                <ambientLight intensity={0.5} />
+                <Suspense fallback={null}>
+                    <Arch />
+                </Suspense>
+                <OrbitControls enablePan={true} enableZoom={true} enableRotate={true} />
+            </Canvas>
+        </>
     )
 }
