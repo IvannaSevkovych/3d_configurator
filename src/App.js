acc296eb47f9afc52a2a59a21333e80c3a7d467c
Ivanna Sevkovych <is@wacg.de>
2021-05-29 10:46:13 +0200

    Add wheels variations

--- src/App.js
+++ src/App.js
@@ -10,9 +10,10 @@ const state = proxy({
     current: null,
     visibility: {
         carpaint_dark: false,
+        wheels: true,
     },
     items: {
-        carpaint: "#FFF27A",
+        carpaint: "#040404",
         plastic: "#040404",
         glass: "#ffffff",
         carpaint_dark: "#040404",
@@ -23,23 +24,24 @@ const state = proxy({
         backlights: "#880D0D",
         plates: "#ffffff",
         mirror: "#858585",
+        wheels: "#858585",
+        wheels_2: "#B9AB27",
     }
 })
 
 function Mini(props) {
     const group = useRef()
     const snap = useSnapshot(state)
-    const { nodes, materials } = useGLTF('/Mini_cooper_opt_attached_collapsed_draco.glb')
+    const { nodes, materials } = useGLTF('/Mini_cooper_opt_attached_modifiers_1_tires_vars_collapsed_draco.glb')
 
     // Make materials one-sided
-    Object.keys(materials).forEach( (key) => {
+    Object.keys(materials).forEach((key) => {
         materials[key].side = 0
     })
 
     const [hovered, set] = useState(null)
 
     useEffect(() => {
-        // document.body.style.cursor = hovered ? "pointer" : "auto"
         const cursor = `<svg width="64" height="64" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0)"><path fill="rgba(255, 255, 255, 0.5)" d="M29.5 54C43.031 54 54 43.031 54 29.5S43.031 5 29.5 5 5 15.969 5 29.5 15.969 54 29.5 54z" stroke="#000"/><g filter="url(#filter0_d)"><path d="M29.5 47C39.165 47 47 39.165 47 29.5S39.165 12 29.5 12 12 19.835 12 29.5 19.835 47 29.5 47z" fill="${snap.items[hovered]}"/></g><path d="M2 2l11 2.947L4.947 13 2 2z" fill="#000"/><text fill="#000" style="white-space:pre" font-family="Inter var, sans-serif" font-size="10" letter-spacing="-.01em"><tspan x="35" y="63">${hovered}</tspan></text></g><defs><clipPath id="clip0"><path fill="#fff" d="M0 0h64v64H0z"/></clipPath><filter id="filter0_d" x="6" y="8" width="47" height="47" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="BackgroundImageFix"/><feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/><feOffset dy="2"/><feGaussianBlur stdDeviation="3"/><feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.15 0"/><feBlend in2="BackgroundImageFix" result="effect1_dropShadow"/><feBlend in="SourceGraphic" in2="effect1_dropShadow" result="shape"/></filter></defs></svg>`
         const auto = `<svg width="64" height="64" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill="rgba(255, 255, 255, 0.5)" d="M29.5 54C43.031 54 54 43.031 54 29.5S43.031 5 29.5 5 5 15.969 5 29.5 15.969 54 29.5 54z" stroke="#000"/><path d="M2 2l11 2.947L4.947 13 2 2z" fill="#000"/></svg>`
         document.body.style.cursor = `url('data:image/svg+xml;base64,${btoa(hovered ? cursor : auto)}'), auto`
@@ -54,73 +56,81 @@ function Mini(props) {
                 onPointerDown={(e) => { e.stopPropagation, state.current = e.object.material.name }}
                 onPointerMissed={(e) => { state.current = null }}
 
-                position={[0.53, 0.16, 0.73]}
-                rotation={[0, 0, -Math.PI / 2]}
-                scale={[0.02, 0.02, 0.02]}>
+                position={[-0.01, -0.04, -0.1]} rotation={[-Math.PI, 0, -Math.PI]}
+            >
+
+                <mesh
+                    geometry={nodes.Mesh001.geometry}
+                    material={materials.carpaint}
+                    material-color={snap.items.carpaint}
+                />
+                <mesh
+                    geometry={nodes.Mesh001_1.geometry}
+                    material={materials.backlights}
+                    material-color={snap.items.backlights}
+                />
+                <mesh
+                    geometry={nodes.Mesh001_2.geometry}
+                    material={materials.carpaint_dark}
+                    material-color={snap.items.carpaint_dark}
+                />
+                <mesh
+                    geometry={nodes.Mesh001_3.geometry}
+                    material={materials.carpaint_light}
+                    material-color={snap.items.carpaint_light}
+                />
+                <mesh
+                    geometry={nodes.Mesh001_4.geometry}
+                    material={materials.chrome}
+                    material-color={snap.items.chrome}
+                />
+                <mesh
+                    geometry={nodes.Mesh001_5.geometry}
+                    material={materials.glass}
+                    material-color={snap.items.glass}
+                />
+                <mesh
+                    geometry={nodes.Mesh001_6.geometry}
+                    material={materials.headlight}
+                    material-color={snap.items.headlight}
+                />
+                <mesh
+                    geometry={nodes.Mesh001_7.geometry}
+                    material={materials.mirror}
+                    material-color={snap.items.mirror}
+                />
+                <mesh
+                    geometry={nodes.Mesh001_8.geometry}
+                    material={materials.plastic}
+                    material-color={snap.items.plastic}
+                />
+                <mesh
+                    geometry={nodes.Mesh001_9.geometry}
+                    material={materials.plates}
+                    material-color={snap.items.plates}
+                />
                 <mesh
                     visible={!snap.visibility.carpaint_dark}
-                    geometry={nodes.mini.geometry}
-                    material={materials.wheels}
-                />
-                <mesh
-                    material-color={snap.items.carpaint}
-                    geometry={nodes.mini_1.geometry}
-                    material={materials.carpaint}
-                />
-                <mesh
-                    visible={snap.visibility.carpaint_dark}
-                    geometry={nodes.mini_2.geometry}
+                    geometry={nodes.Mesh001_10.geometry}
                     material={materials.roof}
+                    material-color={snap.items.roof}
                 />
                 <mesh
-                    material-color={snap.items.plastic}
-                    geometry={nodes.mini_3.geometry}
-                    material={materials.plastic}
-                />
-                <mesh
-                    material-color={snap.items.glass}
-                    geometry={nodes.mini_4.geometry}
-                    material={materials.glass}
-                />
-                <mesh
-                    material-color={snap.items.carpaint_dark}
-                    geometry={nodes.mini_5.geometry}
-                    material={materials.carpaint_dark}
-                />
-                <mesh
-                    material-color={snap.items.carpaint_light}
-                    geometry={nodes.mini_6.geometry}
-                    material={materials.carpaint_light}
-                />
-                <mesh
-                    material-color={snap.items.tyres}
-                    geometry={nodes.mini_7.geometry}
+                    geometry={nodes.Mesh001_11.geometry}
                     material={materials.tyres}
+                    material-color={snap.items.tyres}
                 />
                 <mesh
-                    material-color={snap.items.headlight}
-                    geometry={nodes.mini_8.geometry}
-                    material={materials.headlight}
+                    visible={!snap.visibility.wheels}
+                    geometry={nodes.Mesh001_12.geometry}
+                    material={materials.wheels_2}
+                    material-color={snap.items.wheels_2}
                 />
                 <mesh
-                    material-color={snap.items.chrome}
-                    geometry={nodes.mini_9.geometry}
-                    material={materials.chrome}
-                />
-                <mesh
-                    material-color={snap.items.backlights}
-                    geometry={nodes.mini_10.geometry}
-                    material={materials.backlights}
-                />
-                <mesh
-                    material-color={snap.items.plates}
-                    geometry={nodes.mini_11.geometry}
-                    material={materials.plates}
-                />
-                <mesh
-                    material-color={snap.items.mirror}
-                    geometry={nodes.mini_12.geometry}
-                    material={materials.mirror}
+                    visible={snap.visibility.wheels}
+                    geometry={nodes.Mesh001_13.geometry}
+                    material={materials.wheels}
+                    material-color={snap.items.wheels}
                 />
             </group>
         </group>
@@ -145,13 +155,14 @@ function RoofToggler() {
     const snap = useSnapshot(state)
     return (
         <div
-            className="roof"
+            className="xzibit"
             onPointerDown={(e) => {
                 e.stopPropagation;
-                state.visibility.carpaint_dark = !snap.visibility.carpaint_dark;
+                state.visibility.wheels = !snap.visibility.wheels;
             }}
         >
-            <h2>Roof</h2>
+            <img src="/xzibit.jpg"/>
+            <h2>Pimp my ride!</h2>
         </div>
     )
 }
@@ -167,9 +178,6 @@ export default function App() {
                     position={[2, 10, 1]}
                     angle={0.2}
                     penumbra={1}
-                // shadow-mapSize-width={2048}
-                // shadow-mapSize-height={2048}
-                // castShadow
                 />
                 <Suspense fallback={null}>
                     <Mini />
